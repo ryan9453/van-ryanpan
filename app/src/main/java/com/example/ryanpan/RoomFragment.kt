e4a4b8493ba2840bc63c372fc1c272688f399030
Ryan_Pan <huk19920303@gmail.com>
2022-03-17 17:51:18 +0800

    init commit

---
+++ app/src/main/java/com/example/ryanpan/RoomFragment.kt
@@ -0,0 +1,9 @@
+package com.example.ryanpan
+
+import androidx.fragment.app.Fragment
+
+class RoomFragment : Fragment() {
+    companion object {
+        val TAG = RoomFragment::class.java.simpleName
+    }
+}
